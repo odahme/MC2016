0cb35d278dbdccb4f80311cefdddb804b522f0bb
Oliver Dahme <o.dahme@gmail.com>
2016-04-30 08:35:40 +0200

    update

--- Ex8/ex.cpp
+++ Ex8/ex.cpp
@@ -26,36 +26,44 @@ int main(int argc, char **argv)
 
   int number=atoi(argv[1]);
 
-double x = 0;
-int state = 1;
+double x[3] = {0,0,0};
 
-double h[3][4] = {{0.2,0.4,.0.1,0.3},{0.3,0.3,0.1,0.3},{0.1,0.2,0.1,0.6}}
-double a[3] = {1.5,-1.,0.7}
 
-while (state != 3) {
-  double temp = 0;
-  double beforstate = state;
-  for (int i = 0; i < 4; i++) {
-    temp += h[state][i];
-    double r = rand->Uniform(0,1);
-    if (r < temp) {
-      state = i;
-      break;
+double h[3][4] = {{0.2,0.4,0.1,0.3},{0.3,0.3,0.1,0.3},{0.1,0.2,0.1,0.6}};
+double a[3] = {1.5,-1.0,0.7};
+
+for (size_t i = 0; i < number; i++) {
+  double q = rand->Uniform(0,1);
+  int state = -1;
+  double prob = 0;
+  while (q > prob) {
+    state++;
+    prob += 1./3;
+  }
+
+  int beforstate = state;
+  int startstate = state;
+  while (state != 3) {
+    double temp = 0;
+    beforstate = state;
+    for (int i = 0; i < 4; i++) {
+      temp += h[state][i];
+      double r = rand->Uniform(0,1);
+      if (r < temp) {
+        state = i;
+        break;
+      }
     }
   }
+  state = beforstate;
+
+   x[startstate] += 3*a[state] / h[state][3];
 }
-state = beforstate;
 
-double x += a[state] / h[state][3]
-
-
-
-
-
-
-double calc = x
-
-std::cout << calc << std::endl;
+for (size_t i = 0; i < 3; i++) {
+  x[i] = x[i]/number;
+  std::cout << x[i] << std::endl;
+}
 
   //turns off the program with mous clic
 //  theApp.Connect("TCanvas","Closed()","TApplication",&theApp,"Terminate()");
